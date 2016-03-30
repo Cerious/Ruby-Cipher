1468d67d259448caac1c10c129951da9974dc082
cerious <your.ceriousn@yahoo.com>
2016-03-30 16:31:21 -0400

    Finished move_down method and cut count method. Started on out_letter

--- Deck.rb
+++ Deck.rb
@@ -6,7 +6,7 @@ class Deck
 
 	def move_A(int)
 		@deck.delete("A")
-		@deck.insert(int, "B")
+		@deck.insert(int, "A")
 	end
 
 	def move_B(int)
@@ -15,6 +15,7 @@ class Deck
 	end
 
 	def triple_cut
+		move_down
 		a = @deck.index("A")
 		b = @deck.index("B")
 		abv_b = 0..b-1
@@ -28,11 +29,72 @@ class Deck
 		return @deck
 	end
 
-	def move_down(char, int)
-		dwn = @deck.index(char) - int
-		move_A(dwn)
+	def move_down
+		if @deck.last == "A"
+			move_A(1)
+		else
+			dwn = @deck.index(char) + 1
+			move_A(dwn)
+		end
+
+		if @deck.last == "B"
+			moveB(2)
+		else
+			dwn = @deck.index(char) + 2
+			move_B(dwn)
+		end
 	end
 
-	
+	def cut_count
+		val = @deck.last
+		if @deck.last == "A" or "B"
+			val = 53
+		end
+
+		arr = @deck.slice(@deck.index(0..val))
+		@deck.slice!(@deck.index(0..val))
+		@deck = arr.concat(@deck)
+		return @deck 
+	end
+
+	def out_letter
+		@deck[@deck[0] - 1]
+
+		let_to_num = {
+			1 => "A"
+			2 => "B"
+			3 =>"C"
+			4 =>"D"
+			5 =>"E"
+			6 =>"F"
+			7 =>"G"
+			8 =>"H"
+			9 =>"I"
+			10 =>"J"
+			11 =>"K"
+			12 =>"L"
+			13 =>"M"
+			14 =>"N"
+			15 =>"O"
+			16 =>"P"
+			17 =>"Q"
+			18 =>"R"
+			19 =>"S"
+			20 =>"T"
+			21 =>"U"
+			22 =>"V"
+			23 =>"W"
+			24 =>"X"
+			25 =>"Y"
+			26 =>"Z"
+		}
+
+	end
+
+
+
+
 
 end
+
+
