f95d5c140bde0d259c606507a6cd8ac751527b7b
cerious <your.ceriousn@yahoo.com>
2016-04-04 20:45:42 -0400

    All methods except out_letter and cut_count tested and working.

--- Deck.rb
+++ Deck.rb
@@ -1,5 +1,5 @@
-class Deck
-
+class Deck 
+	
 	def initialize 
 		@deck = (1..52).to_a << "A" << "B"
 	end
@@ -15,39 +15,44 @@ class Deck
 	end
 
 	def triple_cut
-		move_down
+		move_downA
+		move_downB
 		a = @deck.index("A")
 		b = @deck.index("B")
-		abv_b = 0..b-1
-		bel_a = a-x+1..@deck.index(array.last)
-		take_b = @deck.slice(abv_b)	
-		array.slice!(abv_b)
-		take_a = array.slice(bel_a)
-		array.slice!(bel_a)
-		@deck = take_b.concat(@deck)
+		abv_b = (0..b-1)
+		x = abv_b.size	
+		bel_a = (a-x+1)..@deck.index(@deck.last)
+		take_b = @deck.slice(abv_b)	 
+		@deck.slice!(abv_b)
+		take_a = @deck.slice(bel_a)
+		@deck.slice!(bel_a)
+		@deck = @deck.concat(take_b)
 		take_a.each {|i| @deck << i}
 		return @deck
 	end
 
-	def move_down
-		if @deck.last == "A"
-			move_A(1)
-		else
-			dwn = @deck.index(char) + 1
-			move_A(dwn)
-		end
+	def move_downA
+		move_A(@deck.index("A") + 1)
+	end
 
+	def move_downB
 		if @deck.last == "B"
-			moveB(2)
+			move_B(2)
+		elsif @deck[@deck.index(@deck.last) - 1] == "B"
+			move_B(1)
 		else
-			dwn = @deck.index(char) + 2
+			dwn = @deck.index("B") + 2
 			move_B(dwn)
 		end
+
 	end
 
 	def cut_count
+		triple_cut
 		val = @deck.last
-		if @deck.last == "A" or "B"
+		a = "A"
+		b = "B"
+		if @deck.last == a or b
 			val = 53
 		end
 
@@ -60,40 +65,21 @@ class Deck
 	def out_letter
 		val = @deck[@deck[0] - 1]
 
-		let_to_num = {
-			1 => "A"
-			2 => "B"
-			3 =>"C"
-			4 =>"D"
-			5 =>"E"
-			6 =>"F"
-			7 =>"G"
-			8 =>"H"
-			9 =>"I"
-			10 =>"J"
-			11 =>"K"
-			12 =>"L"
-			13 =>"M"
-			14 =>"N"
-			15 =>"O"
-			16 =>"P"
-			17 =>"Q"
-			18 =>"R"
-			19 =>"S"
-			20 =>"T"
-			21 =>"U"
-			22 =>"V"
-			23 =>"W"
-			24 =>"X"
-			25 =>"Y"
-			26 =>"Z"
-		}
+		let_to_num = { 1 => 'A', 2 =>  'B', 3 => 'C', 4 => 'D', 5 => 'E', 6 => 'F', 7 => 'G', 8 => 'H', 9 => 'I', 10 => 'I', 11 => 'K', 12 => 'L', 13 => 'M', 14 => 'N', 15 => 'O', 16 => 'P', 17 => 'Q', 18 => 'R', 19 => 'S', 20 => 'T', 21 => 'U', 22 => 'V', 23 => 'W', 24 => 'X', 25 => 'Y', 26 => 'Z'}
 
 		out = []
-		out << let_to_num[val]
+		out << let_to_num[@deck[val]]
 
 	end
-
+	
+	def deck_p                       ## For test purposes only.
+		return @deck
+	end
 end
+
+yugi = Deck.new
+print yugi.cut_count
+
+
 
 
